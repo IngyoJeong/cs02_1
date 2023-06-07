c08e0533112f77937722e9c7a3783ff6bbb77548
IngyoJeong <127263653+IngyoJeong@users.noreply.github.com>
2023-06-07 10:56:08 +0900

    Add files via upload

---
+++ 018_ClassInCs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _018_ClassInCs
+{
+    class product
+    {
+        public string name;
+        public int price;
+    }
+
+    class MyMath
+    {
+        public static double PI = 3.14; //static 정적 변수(클래스 변수)
+    }
+
+    class MyCalendar
+    {
+        public const int months = 12;
+        public const int weeks = 52;
+        public const int days = 365;
+
+        public const double daysPerWeek = (double)days / (double)weeks;
+        public const double daysPerMonth = (double)days / (double)months;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            product p = new product();
+            p.name = "시계";
+            p.price = 100000;
+
+            Console.WriteLine("{0} : {1:C}", p.name, p.price); //객체.값
+            Console.WriteLine("원주율 : {0}", MyMath.PI); //static.값 객체를 안만들고 클래스 이름으로 써도됨
+            Console.WriteLine("한 달은 평균 {0:F3}일", MyCalendar.daysPerMonth);
+        }
+    }
+}
---
+++ 020_Construct/Date.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _020_Construct
+{
+    internal class Date
+    {
+        private int year, month, day;
+
+        public Date()
+        {
+            year = 1; //this 생략
+            month = 1; //this 생략
+            day = 1; //this 생략
+        }
+
+        public Date(int y, int m, int d)
+        {
+            year = y; //this 생략
+            month = m; //this 생략
+            day = d; //this 생략
+        }
+
+        public void PrintDate()
+        {
+            Console.WriteLine("{0}/{1}/{2}", year, month, day);
+        }
+    }
+}
